a15a3f5ac81d48a945fc5b62244589df7d7fc291
Bipin761 <bipinkumardehariya@gmail.com>
2025-01-12 22:31:51 +0530

    Fixed: Corrected the directories and included an index page under '{IP Address}/HABIT'

--- habitTracker/habitTracker/urls.py
+++ habitTracker/habitTracker/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('HABIT/', include(HABIT.urls))
+    path('HABIT/', include('HABIT.urls'))
 ]
