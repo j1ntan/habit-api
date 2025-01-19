d7ba7ff0a6f67c259b10b30c43a8fe187576d8d2
Bipin761 <bipinkumardehariya@gmail.com>
2025-01-19 18:06:20 +0530

    ADD: Added the remaining three models to habit_models/models.py

--- habitTracker/habit_models/models.py
+++ habitTracker/habit_models/models.py
@@ -26,3 +26,22 @@ class HabitProgress(models.Model):
     date = models.DateField()
     completed = models.BooleanField(default=False)
     completion_percentage = models.IntegerField(default=0)
+
+class Streak(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    habit = models.ForeignKey(Habit, on_delete = models.CASCADE)
+    streak_count = models.IntegerField(default = 0)
+    last_completed = models.DateField(auto_now = True)
+
+class Analytics(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    habit = models.ForeignKey(Habit, on_delete = models.CASCADE)
+    weekly_completion_rate = models.FloatField(default = 0)
+    monthly_completion_rate = models.FloatField(default = 0)
+    best_habit = models.ForeignKey(Habit, null = True, blank = True, on_delete = models.SET_NULL, related_name = "best_habit_analytics")
+    worst_habit = models.ForeignKey(Habit, null = True, blank = True, on_delete = models.SET_NULL, related_name = "worst_habit_analytics")
+
+class Calender(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    date = models.DateField()
+    completed_habits = models.ManyToManyField(Habit)
