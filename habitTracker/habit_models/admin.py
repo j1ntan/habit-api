92e007c702299500ac5329bc123aba59fda8d5b5
Bipin761 <bipinkumardehariya@gmail.com>
2025-01-19 18:09:34 +0530

    CORRECTION: Registered models onto admin panel

--- habitTracker/habit_models/admin.py
+++ habitTracker/habit_models/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User, Habit, HabitProgress
+from .models import *
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Habit)
 admin.site.register(HabitProgress)
+admin.site.register(Streak)
+admin.site.register(Analytics)
+admin.site.register(Calender)
