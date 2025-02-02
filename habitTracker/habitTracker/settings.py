b468880da923b8cc9b1d18172d38e25b672c442c
Bipin761 <bipinkumardehariya@gmail.com>
2025-02-02 12:51:59 +0530

    Added remaining settings related to cors in settings.py

--- habitTracker/habitTracker/settings.py
+++ habitTracker/habitTracker/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
 
 ROOT_URLCONF = 'habitTracker.urls'
 
